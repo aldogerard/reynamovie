6db1dff7b378bf7001b441cba3e125612a699f5b
aldogerard <aldogerard12@gmail.com>
2023-04-23 05:23:13 +0700

    changes title

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ const header = ({ setSearch, onSubmit }) => {
   return (
     <>
       <div className="py-4">
-        <h1 className="text-white text-5xl py-4 text-center font-semibold bg-gradient-to-r from-amber-500 to-rose-500 bg-clip-text text-transparent">ReyNa Movie's</h1>
+        <h1 className="text-white text-5xl py-4 text-center font-semibold bg-gradient-to-r from-amber-500 to-rose-500 bg-clip-text text-transparent">ReyNa Movies</h1>
         <div className="flex justify-center py-2 gap-2 max-w-lg sm:max-w-2xl mx-auto">
           <input
             type="text"
